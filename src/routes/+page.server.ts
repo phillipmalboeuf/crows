4ebd48437bba4abcca3ff8181c000531ce8600fe
Phillip Malboeuf <phil@phils.computer>
2025-10-10 18:32:50 -0400

    ready filter

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,7 @@
 import { shopify } from '$lib/clients/shopify';
-import { getOrders } from '$lib/services/orders';
-import { getMaterials, getProjects, getVariants } from '$lib/services/materials';
-import type { Material, Project, Variant } from '$lib/clients/schema';
+import { getOrders  } from '$lib/services/orders';
+import { getAssignedOrders, getMaterials, getProjects, getVariants } from '$lib/services/materials';
+import type { Material, Project, Variant, Order } from '$lib/clients/schema';
 
 
 export const load = async ({ request }) => {
@@ -11,9 +11,14 @@ export const load = async ({ request }) => {
     getProjects(),
     getMaterials()
   ])
+
+  const assignments = (await getAssignedOrders(orders.map(order => order.name))).reduce((acc, assignment) => ({
+      ...acc,
+      [assignment.order]: assignment
+    }), {} as Record<string, Order>)
   
   return {
-    orders,
+    orders: orders.filter(order => !assignments[order.name]?.ready),
     variants: variants.reduce((acc, variant) => ({
       ...acc,
       [variant.shopify_variant]: {
